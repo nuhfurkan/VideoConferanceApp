6b3d09a719b71aba3e93de97c3aff0aae70770d2
Furkan Erturk <furkanerturk@Furkan-MacBook-Air.local>
2021-11-28 00:39:15 +0200

    uncompleted tasks exist

--- public/client.js
+++ public/client.js
@@ -3,27 +3,120 @@ const ClieIO = io();
 const myVideo = document.getElementById("my-video");
 const remoteVideo = document.getElementById("remote-video");
 const inputId = document.getElementById("room-id");
+var remoteStream;
+var localStream;
+var myrtcpeer;
 
 const constraints = {
     audio: true,
     video: {height: 300, width:300}
 }
 
+ClieIO.on("joined_room", (roomid)=> {
+    //stream("remote-video");
+    
+    ClieIO.emit("start_call", roomid);
+})
+
+ClieIO.on("start_call", (roomid)=> {
+  console.log("all is well");
+  myrtcpeer = new RTCPeerConnection();
+  addLocalTrack(myrtcpeer);
+  myrtcpeer.ontrack = setRemoteStream;
+  myrtcpeer.onicecandidate = sendIceCandidate;
+  createOffer(myrtcpeer);
+})
+
+ClieIO.on("web_rtc_icecandidate", event => {
+  console.log("remote connection");
+})
+
+ClieIO.on("webrtc_offer", event => {
+  console.log("webrtc_oofer");
+  myrtcpeer = new RTCPeerConnection();
+  addLocalTrack(myrtcpeer);
+  myrtcpeer.ontrack = setRemoteStream;
+  myrtcpeer.onicecandidate = sendIceCandidate;
+  createOffer(myrtcpeer);
+})
+
+ClieIO.on("webrtc_answer", event => {
+  myrtcpeer.setRemoteDesc(new RTCSessionDescription(event));
+})
+
+function addLocalTrack() {
+  localStream.getTracks().forEach(track => {
+    myrtcpeer.addTrack(track);
+  });
+}
+
+async function createAnswer(rtypeercon) {
+  let sesDesc;
+  try {
+    sesDesc = await rtypeercon.createAnswer();
+    // no local desc
+    //rtypeercon.setLocalDesc(sesDesc);
+  } catch (error) {
+    console.error(error);
+  }
+  ClieIO.emit("webrtc_answer", {
+    type: "webrtc_answer",
+    sdp: sesDesc,
+    roomid: inputId.value 
+  })
+
+}
+
+async function createOffer(rtypeercon) {
+  let sesDesc;
+  try {
+    sesDesc = await rtypeercon.createOffer();
+    // no local desc
+    //rtypeercon.setLocalDesc(sesDesc);
+  } catch (error) {
+    console.error(error);
+  }
+  ClieIO.emit("webrtc_offer", {
+    type: "webrtc_offer",
+    sdp: sesDesc,
+    roomid: inputId.value 
+  })
+
+}
+
+function setRemoteStream(event) {
+  remoteVideo.srcObject = event.streams[0];
+  remoteStream = event.stream;
+  remoteVideo.play();
+}
+
+function sendIceCandidate(event) {
+  if (event.candidate) {
+    ClieIO.emit("web_rtc_icecandidate", {
+      roomid: inputId.value,
+      lanel: event.candidate.sdpMLineIndex,
+      candidate: event.candidate.candidate
+    });
+
+  }
+}
+
 function joinRoom() {
     console.log(inputId.value);
     if (inputId.value != "") {
-        ClieIO.emit("join_room", inputId.value)    
-        stream()
+        ClieIO.emit("join_room", inputId.value);  
+        stream("my-video");
     } else {
         alert("Room can not be empty");
     }
 }
 
-async function stream() {
-    const video = document.getElementById("my-video");
+async function stream(myid) {
+    const video = document.getElementById(myid);
     try {
       const mediaStream = await navigator.mediaDevices.getUserMedia(constraints)
       video.srcObject = mediaStream;
+      localStream = mediaStream;
     } catch (e) {
       console.error(e)
     }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const http = require("http")
+const { Socket } = require("socket.io")
 const server = require("http").createServer(app)
 const io = require("socket.io")(server)
 app.use("/", express.static("public"))
@@ -10,6 +11,23 @@ io.on("connection", (server)=> {
     server.on("join_room", (roomid)=> {
         console.log("In room " + roomid);
         server.join([roomid]);
+        server.emit("joined_room", roomid);
+    })
+    
+    server.on("start_call", (roomid)=> {
+        server.broadcast.to(roomid).emit("start_call");
+    })
+
+    server.on("webrtc_offer", event => {
+        server.broadcast.to(event.roomid).emit("webrtc_offer", event);
+    })
+
+    server.on("webrtc_answer", event => {
+        server.broadcast.to(event.roomid).emit("webrtc_answer", event.sdp);
+    })
+
+    server.on("web_rtc_icecandidate", event => {
+        server.broadcast.to(event.roomid).emit("web_rtc_icecandidate", event);
     })
 })
 server.listen(3000, ()=> {
